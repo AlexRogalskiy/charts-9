d0cd6e5c24a33fbe0882e95ecf34607625906812
rob boll <robertcboll@gmail.com>
2016-11-13 16:30:33 -0500

    updates

--- charts/elasticsearch/images/build-push.sh
+++ charts/elasticsearch/images/build-push.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-es_image=quay.io/roboll/elasticsearch-kubernetes:2.4.1
+es_image=quay.io/roboll/elasticsearch-kubernetes:v2.4.1
 docker build -t $es_image $dir/elasticsearch-kubernetes
 docker push $es_image
 
-metrics_image=quay.io/roboll/elasticsearch-metrics:0.3.0
+metrics_image=quay.io/roboll/elasticsearch-metrics:v0.3.0
 docker build -t $metrics_image $dir/elasticsearch-metrics
 docker push $metrics_image
--- charts/fluentd/images/build-push.sh
+++ charts/fluentd/images/build-push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-fluentd_image=quay.io/roboll/fluentd-kubernetes:2.4.1
+fluentd_image=quay.io/roboll/fluentd-kubernetes:v1.19
 docker build -t $fluentd_image $dir/fluentd-kubernetes
 docker push $fluentd_image
